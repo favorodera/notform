3519c4055b6c59e66be4d3cba27894f19ddf3e41
Favour  Emeka <favorodera@gmail.com>
2026-01-14 21:20:05 +0100

    feat(project): scaffold new Vue 3 project with Vite, TS, ESLint, Vitest, Playwright and pnpm

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'valid',
+      fileName: format => `valid.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue', 'zod'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          zod: 'Zod',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
