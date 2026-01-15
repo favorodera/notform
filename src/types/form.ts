ce62c928833b7ac91c44801b63746b2bf45b0002
Favour  Emeka <favorodera@gmail.com>
2026-01-15 12:38:39 +0100

    feat(form): add Form and FormProps types

---
+++ src/types/form.ts
@@ -0,0 +1,39 @@
+import { type ComputedRef } from 'vue'
+import * as zod from 'zod/v4/core'
+import type { Schema, ValidationMode, ValidationTriggers } from './shared'
+import type { DeepPartial } from './utils'
+
+/**
+ * The context the form is running on.
+ * Provides access to form state, errors, and validation methods.
+ *
+ * @template TSchema - The zod schema of the form
+ */
+ 
+export type Form<TSchema extends Schema = Schema> = {
+  /** Reactive reference to the current form state (values) */
+  state: ComputedRef<DeepPartial<zod.output<TSchema>> | zod.output<TSchema>>
+
+  /** Reactive reference to the current form errors */
+  errors: ComputedRef<zod.$ZodErrorTree<TSchema>['properties']>
+
+  /** The validation mode of the form
+   * @see {@link ValidationMode}
+   * @defaultValue 'lazy'
+   */
+  mode: ValidationMode
+
+  /** The validation triggers of the form
+   * @see {@link ValidationTriggers}
+   * @defaultValue ['blur', 'input']
+   */
+  validateOn: ValidationTriggers[]
+
+  /** Validate the form */
+  validateForm(): Promise<boolean>
+}
+
+export type FormProps<TSchema extends Schema = Schema> = Partial<Pick<Form<TSchema>, 'mode' | 'validateOn'>> & {
+  schema: TSchema
+  state: DeepPartial<zod.output<TSchema>> | zod.output<TSchema>
+}
