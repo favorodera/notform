34787d0645a50756e1810f0b02bce73784a3a340
Favour  Emeka <favorodera@gmail.com>
2026-01-15 22:51:23 +0100

    feat(types): export utility types for input/output inference

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export type { Form, FormProps, FormEmits } from './types/form'
 export type { ValidationMode, ValidationTriggers, Schema } from './types/shared'
+export type { InferInput, InferOutput, OutputPathValue, InputPathValue, InputPaths, OutputPaths } from './types/utils'
