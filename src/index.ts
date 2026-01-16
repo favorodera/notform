e9b7f8a707697fae8ad84c5c50ec35b31a0477e4
Favour  Emeka <favorodera@gmail.com>
2026-01-16 20:03:27 +0100

    feat(exports): expose additional form types and update component export to named

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export type { Form, FormProps, FormEmits } from './types/form'
+export type { Form, FormProps, FormEmits, FormExpose, FormSlots, SubmitResult } from './types/form'
 export type { ValidationMode, ValidationTriggers, Schema } from './types/shared'
 export type { InferInput, InferOutput, OutputPathValue, InputPathValue, InputPaths, OutputPaths } from './types/utils'
 
 
-export { default } from './components/valid-form.vue'
+export { default as ValidForm } from './components/valid-form.vue'
