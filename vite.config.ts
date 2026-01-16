d0d2ae0fa244a70cc23b9bfb06745606d7b61698
Favour  Emeka <favorodera@gmail.com>
2026-01-16 05:35:41 +0100

    refactor(build): streamline build process and update test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
+  test: {
+    reporters: ['default', 'html'],
+    outputFile: './.vitest/report.html',
+    typecheck: { enabled: true },
+    projects: [
+      {
+        test: {
+          name: 'unit',
+          root: './src',
+          environment: 'node',
+          include: ['../src/tests/unit/*.test.ts'],
+        },
+      },
+    ],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
