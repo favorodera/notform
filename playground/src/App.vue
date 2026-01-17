f6cc71819dc66d28b1bb87c1b226978b8fb6028a
Favour  Emeka <favorodera@gmail.com>
2026-01-17 22:43:47 +0100

    chore(deps): move zod from devDependencies to dependencies

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-import { MyButton } from '../../src'
+import { reactive, useTemplateRef } from 'vue'
+import { ValidForm, ValidField } from '../../src'
+import { z } from 'zod'
+
+const schema = z.object({
+  name: z.string().min(3),
+})
+
+const state = reactive({
+  name: '',
+})
+
+const form = useTemplateRef('form')
 </script>
 
 <template>
-  <MyButton type="primary" />
+  <ValidForm ref="form" :schema="schema" :state="state">
+
+    <ValidField name="name" v-slot="{ field }">
+
+      <input type="text" v-bind="field" />
+
+    </ValidField>
+
+     <pre>{{ JSON.stringify(form, null, 2) }}</pre>
+  </ValidForm>
 </template>
