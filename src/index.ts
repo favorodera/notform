87bad60276f1e0e70daa53b42e038f5ab361edca
Favour  Emeka <favorodera@gmail.com>
2026-01-17 10:23:21 +0100

    refactor(form): update form types import path to valid-form

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { Form, FormProps, FormEmits, FormExpose, FormSlots, SubmitResult } from './types/form'
+export type { Form, FormProps, FormEmits, FormExpose, FormSlots, SubmitResult } from './types/valid-form'
 export type { ValidationMode, ValidationTriggers, Schema } from './types/shared'
 export type { InferInput, InferOutput, OutputPathValue, InputPathValue, InputPaths, OutputPaths } from './types/utils'
 
