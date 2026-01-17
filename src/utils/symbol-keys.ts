d0b4719a11fdc1814a07401cd920b2828ff17757
Favour  Emeka <favorodera@gmail.com>
2026-01-17 10:26:03 +0100

    refactor(types): update Form type import path

--- src/utils/symbol-keys.ts
+++ src/utils/symbol-keys.ts
@@ -1,5 +1,5 @@
 import type { InjectionKey } from 'vue'
-import type { Form } from '../types/form'
+import type { Form } from '../types/valid-form'
 
 /** The key used to inject the form factory */
 export const formFactoryKey = Symbol('valid:form-factory') as InjectionKey<Form>
