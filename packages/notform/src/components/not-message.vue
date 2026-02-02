3c75a150e60ff695212d1007bb57d1beea4776b0
Favour Emeka <favorodera@gmail.com>
2026-02-02 20:03:52 +0100

    feat(not-message): add 'as' prop and propagate attributes to root element

--- packages/notform/src/components/not-message.vue
+++ packages/notform/src/components/not-message.vue
@@ -1,19 +1,24 @@
 <script lang="ts" setup generic="TSchema extends ObjectSchema">
 import { type NotMessageContext, type NotMessageProps, type NotMessageSlots } from '../types/not-message'
 import type { ObjectSchema } from '../types/shared'
-import { computed, inject, reactive } from 'vue'
+import { computed, inject, reactive, useAttrs } from 'vue'
 import { CURRENT_NOT_FORM_ID_KEY, withContext } from '../utils/not-form-context'
 
 /**
  * Displays validation error messages for a specific form field.
  * @template TSchema The validation schema type derived from ObjectSchema.
  */
-const props = defineProps<NotMessageProps>()
+const props = withDefaults(defineProps<NotMessageProps>(), {
+  as: 'span',
+})
 
 /**
  * Slots provided by the Message component.
  */
 defineSlots<NotMessageSlots>()
+
+// Retrieve attributes from the parent component
+const attributes = useAttrs()
 
 // Retrieve the ID of the form this message belongs to
 const formID = inject(CURRENT_NOT_FORM_ID_KEY)
@@ -33,10 +38,22 @@ const context = reactive({
   message: computed(() => getFieldErrors(props.name)
     .map(error => error.message)[0]),
 }) as NotMessageContext
+
+// Merge attributes with context, ensuring props.id takes precedence
+const mergedAttrs = computed(() => ({
+  ...context,
+  attributes,
+}))
 </script>
 
 <template>
-  <slot v-bind="context">
-    <span v-show="context.message">{{ context.message }}</span>
+  <slot v-bind="mergedAttrs">
+    <component
+      :is="props.as"
+      v-bind="attributes"
+      v-show="context.message"
+    >
+      {{ context.message }}
+    </component>
   </slot>
 </template>
