5ba4a51ec765535d06c5ca236a43a07accb077b5
Favour Emeka <favorodera@gmail.com>
2026-04-09 22:33:50 +0100

    feat: export useNotForm composable and NotForm component types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,9 @@ export type * from './types/shared'
 
 // useNotForm composable types
 export type * from './types/use-not-form'
+
+// useNotForm composable
+export * from './composables/use-not-form'
+
+// NotForm component types
+export type * from './types/not-form'
