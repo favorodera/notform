26b5606e7a34bd6a30bd5b6333eed50d820892d1
Favour Emeka <favorodera@gmail.com>
2026-04-10 19:18:38 +0100

    feat: enhance not-field component with validation, touch, and dirty states

--- packages/core/src/components/not-field.vue
+++ packages/core/src/components/not-field.vue
@@ -1,31 +1,67 @@
-<script setup lang="ts" generic="TSchema extends ObjectSchema">
-import { computed, reactive } from 'vue'
-import type { NotFieldProps, NotFieldSlots } from '../types/not-field'
-import type { ObjectSchema } from '../types/shared'
+<script setup lang="ts" generic="
+  TSchema extends ObjectSchema,
+  TPath extends Paths<StandardSchemaV1.InferInput<TSchema>>
+"
+>
+import { computed, reactive, ref } from 'vue'
+import { getProperty } from 'dot-prop'
+import type { StandardSchemaV1 } from '@standard-schema/spec'
+import type { NotFieldProps, NotFieldSlots, NotFieldInstance } from '../types/not-field'
+import type { ObjectSchema, Paths } from '../types/shared'
 import { useNotFormInstance } from '../utils/instance-utils'
 
-defineOptions({
-  inheritAttrs: false,
-})
+defineOptions({ inheritAttrs: false })
+defineSlots<NotFieldSlots<TSchema, TPath>>()
 
-defineSlots<NotFieldSlots<TSchema>>()
-
-const props = defineProps<NotFieldProps<TSchema>>()
+const props = defineProps<NotFieldProps<TSchema, TPath>>()
 
 
-// BASELINE FORM INSTANCE
+// BASELINE
 const formInstance = useNotFormInstance(props.form)
 
+
+// STATE
+const isValidating = ref(false)
+
 const path = computed(() => props.path)
-const errors = formInstance.getFieldErrors(path.value)
-const validate = () => formInstance.validateField(path.value)
+const value = computed(() => getProperty(formInstance.values.value, props.path) as NotFieldInstance<TSchema, TPath>['value'])
+const errors = computed(() => formInstance.getFieldErrors(path.value))
+
+
+// TOUCH
+const touch = () => formInstance.touchField(path.value)
+const unTouch = () => formInstance.unTouchField(path.value)
+
+
+// DIRTY
+const dirty = () => formInstance.dirtyField(path.value)
+const unDirty = () => formInstance.unDirtyField(path.value)
+
+
+// VALIDATION
+const validate = async () => {
+  isValidating.value = true
+  try {
+    return await formInstance.validateField(path.value)
+  }
+  finally {
+    isValidating.value = false
+  }
+}
+
 
 // INSTANCE
 const fieldInstance = reactive({
-  path: path.value,
-  errors: errors,
+  path,
+  value,
+  isValidating,
+  errors,
   validate,
-})
+  touch,
+  unTouch,
+  dirty,
+  unDirty,
+}) as NotFieldInstance<TSchema, TPath>
 </script>
 
 <template>
