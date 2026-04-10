ec65a58a928212f5e43bd3fe97a9e5abceb6ef7d
Favour Emeka <favorodera@gmail.com>
2026-04-10 14:40:32 +0100

    feat: add NotForm component

---
+++ packages/core/src/components/not-form.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts" generic="TSchema extends ObjectSchema">
+import { useAttrs } from 'vue'
+import type { NotFormProps, NotFormSlots } from '../types/not-form'
+import type { ObjectSchema } from '../types/shared'
+import { provideNotFormInstance } from '../utils/instance-utils'
+
+const attributes = useAttrs()
+
+defineSlots<NotFormSlots>()
+
+const props = defineProps<NotFormProps<TSchema>>()
+
+provideNotFormInstance(props.instance)
+</script>
+
+<template>
+  <form v-bind="attributes">
+    <slot />
+  </form>
+</template>
