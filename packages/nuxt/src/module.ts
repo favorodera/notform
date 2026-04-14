da073ce1d198780b1d5a116d31190576565a1522
Favour Emeka <favorodera@gmail.com>
2026-04-14 14:53:11 +0100

    feat: register notform components, composables, and types for Nuxt module

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,19 +1,78 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { addComponent, addImports, addTypeTemplate, createResolver, defineNuxtModule } from '@nuxt/kit'
 
-// Module options TypeScript interface definition
-// export interface ModuleOptions {}
+/** Nuxt module options for `notform` */
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface NotFormModuleOptions {}
 
-export default defineNuxtModule({
+const components = [
+  'NotForm',
+  'NotField',
+  'NotArrayField',
+  'NotMessage',
+] as const
+
+const composables = [
+  'useNotForm',
+] as const
+
+export default defineNuxtModule<NotFormModuleOptions>({
   meta: {
     name: 'notform-nuxt',
     configKey: 'notform',
+    compatibility: {
+      nuxt: '>=4.0.0',
+    },
   },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup() {
-    const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+  // Module factory
+  setup(_options, nuxt) {
+    // Create a resolver
+    const { resolve } = createResolver(import.meta.url)
+
+    // Create a resolver for the runtime files
+    const componentsRuntime = resolve('./runtime/components')
+    const composablesRuntime = resolve('./runtime/composables')
+
+    // Exclude notform package from optimizeDeps
+    nuxt.options.vite.optimizeDeps = nuxt.options.vite.optimizeDeps ||= {}
+    nuxt.options.vite.optimizeDeps.exclude = nuxt.options.vite.optimizeDeps.exclude ||= []
+    if (!nuxt.options.vite.optimizeDeps.exclude.includes('notform')) {
+      nuxt.options.vite.optimizeDeps.exclude.push('notform')
+    }
+
+    // Add components
+    components.forEach((name) => {
+      addComponent({
+        name,
+        export: name,
+        filePath: componentsRuntime,
+      })
+    })
+
+    // Add composables
+    composables.forEach((composable) => {
+      addImports({
+        name: composable,
+        as: composable,
+        from: composablesRuntime,
+      })
+    })
+
+    // Inject notform types into the consuming project
+    addTypeTemplate({
+      filename: 'types/notform.d.ts',
+      getContents: () => 'export type * from \'notform\'',
+      write: true,
+    })
   },
+
 })
+
+declare module '@nuxt/schema' {
+  interface NuxtConfig {
+    notform?: NotFormModuleOptions
+  }
+  interface NuxtOptions {
+    notform?: NotFormModuleOptions
+  }
+}
