8b3a151fc225042e0c0601e6039abeee77f11f55
Favour Emeka <favorodera@gmail.com>
2026-06-18 19:34:37 +0100

    chore(deps): update dependencies and remove migration docs
    - Update @favorodera/eslint-config to 0.1.2
    - Remove obsolete v1 migration documentation
    - Clean up minor formatting and unused ESLint rules

--- apps/docs/eslint.config.ts
+++ apps/docs/eslint.config.ts
@@ -2,22 +2,10 @@ import { factory } from '@favorodera/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(factory({
+  markdown: false,
   tailwind: {
     settings: {
       entryPoint: 'app/assets/css/main.css',
     },
   },
-})
-  .overrides({
-    'favorodera/markdown/code-in-md/disables': {
-      rules: {
-        'unused-imports/no-unused-vars': 'off',
-        'vue/no-unused-vars': 'off',
-      },
-    },
-    'favorodera/markdown/rules': {
-      rules: {
-        'unused-imports/no-unused-vars': 'off',
-      },
-    },
-  }))
+}))
