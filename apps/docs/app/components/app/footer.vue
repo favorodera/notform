31d83e918916879b84c06056a3b0d78ffa8b978b
Favour Emeka <favorodera@gmail.com>
2026-08-26 23:46:51 +0100

    fix(docs): span footer border across full width
    - Wrap container in native footer element
    - Allow top border to stretch across full viewport

--- apps/docs/app/components/app/footer.vue
+++ apps/docs/app/components/app/footer.vue
@@ -1,16 +1,17 @@
 <template>
-  <Container
-    as="footer"
-    class="flex items-center justify-center border-bs border-default py-5"
-  >
-    <span class="text-sm font-light text-muted">
-      Published under <NuxtLink
-        class="font-medium text-default"
-        to="https://github.com/favorodera/notform/blob/main/LICENSE"
-        external
-        rel="noopener noreferrer"
-        target="_blank"
-      >MIT License</NuxtLink>
-    </span>
-  </Container>
+  <footer class="flex items-center justify-center border-bs border-default">
+    <Container
+      class="flex items-center justify-center py-5"
+    >
+      <span class="text-sm font-light text-muted">
+        Published under <NuxtLink
+          class="font-medium text-default"
+          to="https://github.com/favorodera/notform/blob/main/LICENSE"
+          external
+          rel="noopener noreferrer"
+          target="_blank"
+        >MIT License</NuxtLink>
+      </span>
+    </Container>
+  </footer>
 </template>
