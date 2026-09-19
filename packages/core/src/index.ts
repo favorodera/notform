1275627f1233eeac687dee097d3e7d234e3d6695
Favour Emeka <favorodera@gmail.com>
2026-09-19 05:44:05 +0100

    feat(core): export NotArrayField component and types
    - Expose array field component from core entry point
    - Allow consumers to use array field definitions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,6 +6,7 @@
  */
 
 // Components
+export { default as NotArrayField } from './components/not-array-field.vue'
 export { default as NotField } from './components/not-field.vue'
 export { default as NotForm } from './components/not-form.vue'
 export { default as NotMessage } from './components/not-message.vue'
@@ -14,6 +15,7 @@ export { default as NotMessage } from './components/not-message.vue'
 export { default as useNotForm } from './composables/use-not-form'
 
 // Types
+export type * from './types/not-array-field'
 export type * from './types/not-field'
 export type * from './types/not-form'
 export type * from './types/not-form-api'
